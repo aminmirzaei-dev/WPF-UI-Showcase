c76068612fc54df364d34e70dd1f972721437a8d
Amin Mirzaei <aminmirzaeidev@gmail.com>
2026-08-14 16:54:25 +0330

    Clean up using directives and update TVElement & TVStorageDesc class summary

--- sources/TVSettingsUI/Controls/TVStorageDesc.xaml.cs
+++ sources/TVSettingsUI/Controls/TVStorageDesc.xaml.cs
@@ -1,20 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace TVSettingsUI.Controls
 {
     /// <summary>
-    /// Interaction logic for StorageDesc.xaml
+    /// Interaction logic for TVStorageDesc.xaml
     /// </summary>
     public partial class TVStorageDesc : UserControl
     {
