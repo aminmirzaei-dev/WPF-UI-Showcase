482969d56b43d8129d669e4a76cd0bd6d4eba7ee
Amin Mirzaei <aminmirzaeidev@gmail.com>
2026-08-14 16:52:37 +0330

    Add Title and Color dependency properties to TVStorageDesc control in TVSettingsUI

--- sources/TVSettingsUI/Controls/TVStorageDesc.xaml.cs
+++ sources/TVSettingsUI/Controls/TVStorageDesc.xaml.cs
@@ -18,6 +18,23 @@ namespace TVSettingsUI.Controls
     /// </summary>
     public partial class TVStorageDesc : UserControl
     {
+        public static readonly DependencyProperty TitleProperty = DependencyProperty.Register("Title", typeof(string), typeof(TVStorageDesc));
+        public static readonly DependencyProperty ColorProperty = DependencyProperty.Register("Color", typeof(Brush), typeof(TVStorageDesc));
+
+
+        public string Title
+        {
+            get { return (string)GetValue(TitleProperty); }
+            set { SetValue(TitleProperty, value); }
+        }
+
+        public Brush Color
+        {
+            get { return (Brush)GetValue(ColorProperty); }
+            set { SetValue(ColorProperty, value); }
+        }
+
+
         public TVStorageDesc()
         {
             InitializeComponent();
