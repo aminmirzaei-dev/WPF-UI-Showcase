f579ca38b4f2a9336a63cea6dffd6b5632092e44
Amin Mirzaei <aminmirzaeidev@gmail.com>
2026-08-14 16:20:52 +0330

    Update TVElement logic (Add bases method & properties & fields) in TVSettingsUI

--- sources/TVSettingsUI/Controls/TVElement.xaml.cs
+++ sources/TVSettingsUI/Controls/TVElement.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MahApps.Metro.IconPacks;
+using System;
 using System.Collections.Generic;
 using System.Text;
 using System.Windows;
@@ -10,15 +11,51 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Xml.Linq;
 
 namespace TVSettingsUI.Controls
 {
     /// <summary>
     /// Interaction logic for Element.xaml
     /// </summary>
-    public partial class Element : UserControl
+    public partial class TVElement : UserControl
     {
-        public Element()
+
+        public static readonly DependencyProperty TitleProperty = DependencyProperty.Register("Title", typeof(string), typeof(TVElement));
+        public static readonly DependencyProperty IsActiveProperty = DependencyProperty.Register("IsActive", typeof(bool), typeof(TVElement));
+        public static readonly DependencyProperty IconProperty = DependencyProperty.Register("Icon", typeof(PackIconMaterialKind), typeof(TVElement));
+
+
+        public static readonly RoutedEvent ElementClickEvent =
+            EventManager.RegisterRoutedEvent("ElementClick", RoutingStrategy.Bubble, typeof(RoutedEventHandler), typeof(TVElement));
+
+        public event RoutedEventHandler ElementClick
+        {
+            add { AddHandler(ElementClickEvent, value); }
+            remove { RemoveHandler(ElementClickEvent, value); }
+        }
+
+        void ParentButton_Click(object sender, RoutedEventArgs e) => RaiseEvent(new RoutedEventArgs(ElementClickEvent));
+
+        public string Title
+        {
+            get { return (string)GetValue(TitleProperty); }
+            set { SetValue(TitleProperty, value); }
+        }
+
+        public bool IsActive
+        {
+            get { return (bool)GetValue(IsActiveProperty); }
+            set { SetValue(IsActiveProperty, value); }
+        }
+
+        public PackIconMaterialKind Icon
+        {
+            get { return (PackIconMaterialKind)GetValue(IconProperty); }
+            set { SetValue(IconProperty, value); }
+        }
+
+        public TVElement()
         {
             InitializeComponent();
         }
