0a6ed518a674631f989a31dab87a70dff358f178
Amin Mirzaei <aminmirzaeidev@gmail.com>
2026-08-15 21:13:21 +0330

    Add LanguageService and ThemeService; clean up csproj
    Added LanguageService for localization management and ThemeService for dynamic theme switching in the Services namespace. Removed an unused <ItemGroup> referencing the Services folder from TVSettingsUI.csproj.

---
+++ sources/TVSettingsUI/Services/LanguageService.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Globalization;
+using System.Resources;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace TVSettingsUI.Services
+{
+    public class LanguageService
+    {
+        private readonly ResourceManager _resourceManager;
+
+        public LanguageService()
+        {
+            _resourceManager = Languages.Strings.ResourceManager;
+        }
+
+        public string this[string key]
+        {
+            get
+            {
+                return _resourceManager.GetString(
+                    key,
+                    CultureInfo.CurrentUICulture) ?? key;
+            }
+        }
+
+        public void ChangeLanguage(string cultureName)
+        {
+            var culture = new CultureInfo(cultureName);
+
+            CultureInfo.CurrentCulture = culture;
+            CultureInfo.CurrentUICulture = culture;
+
+            OnPropertyChanged(string.Empty);
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        private void OnPropertyChanged(
+            [CallerMemberName] string? propertyName = null)
+        {
+            PropertyChanged?.Invoke(
+                this,
+                new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ sources/TVSettingsUI/Services/ThemeService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Windows;
+
+namespace TVSettingsUI.Services
+{
+    public class ThemeService
+    {
+        public void ApplyTheme(string themeName)
+        {
+            var theme = new ResourceDictionary
+            {
+                Source = new Uri($"/TVSettingsUI;component/Themes/{themeName}.xaml", UriKind.Relative)
+            };
+
+            Application.Current.Resources.MergedDictionaries.Clear();
+            Application.Current.Resources.MergedDictionaries.Add(theme);
+        }
+    }
+}
