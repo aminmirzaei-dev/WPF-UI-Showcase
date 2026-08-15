3c106e08a4e26c70990a77fb82ee869c843000cb
Amin Mirzaei <aminmirzaeidev@gmail.com>
2026-08-15 13:18:01 +0330

    Add icon support to TVStorageDesc using PackIconMaterial
    Enhanced TVStorageDesc to display icons via a new Icon property and updated XAML. Updated Storage.xaml to specify icons for each storage type and added a hard disk icon to "Total Size". Added required XML namespaces for icon usage.

--- sources/TVSettingsUI/Controls/TVStorageDesc.xaml.cs
+++ sources/TVSettingsUI/Controls/TVStorageDesc.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows;
+﻿using MahApps.Metro.IconPacks;
+using System.Text.Json;
+using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Media;
 
@@ -11,12 +13,18 @@ namespace TVSettingsUI.Controls
     {
         public static readonly DependencyProperty TitleProperty = DependencyProperty.Register("Title", typeof(string), typeof(TVStorageDesc));
         public static readonly DependencyProperty ColorProperty = DependencyProperty.Register("Color", typeof(Brush), typeof(TVStorageDesc));
-
+        public static readonly DependencyProperty IconProperty = DependencyProperty.Register("Icon", typeof(PackIconMaterialKind), typeof(TVStorageDesc));
 
         public string Title
         {
             get { return (string)GetValue(TitleProperty); }
             set { SetValue(TitleProperty, value); }
+        }
+
+        public PackIconMaterialKind Icon
+        {
+            get { return (PackIconMaterialKind)GetValue(IconProperty); }
+            set { SetValue(IconProperty, value); }
         }
 
         public Brush Color
