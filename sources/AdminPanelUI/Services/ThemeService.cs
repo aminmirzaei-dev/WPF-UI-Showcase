164784516477ba165dc04b8bc11d1730cb0a7c89
Amin Mirzaei <aminmirzaeidev@gmail.com>
2026-08-27 04:45:27 +0330

    Refactor ThemeService to support theme switching and binding
    Refactored ThemeService to implement INotifyPropertyChanged for property change notifications. Introduced ThemeOptions enum for selectable color themes. Added CurrentTheme property with change notification and logic to apply ResourceDictionaries dynamically. Initialized current theme from application resources and implemented PropertyChanged event for data binding support.

--- sources/AdminPanelUI/Services/ThemeService.cs
+++ sources/AdminPanelUI/Services/ThemeService.cs
@@ -1,11 +1,75 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Text;
+using System.Windows;
 
 namespace AdminPanelUI.Services
 {
-    internal class ThemeService
+    public enum ThemeOptions
     {
+        Blue, Red, Green, Magenta, Orange, Pink
+    }
 
+
+    internal class ThemeService : INotifyPropertyChanged
+    {
+        private ThemeOptions _currentTheme = ThemeOptions.Blue;
+
+        private ResourceDictionary? _currentThemeDictionary;
+
+        public ThemeOptions CurrentTheme
+        {
+            get => _currentTheme;
+
+            private set
+            {
+                if (_currentTheme == value)
+                    return;
+
+                _currentTheme = value;
+
+                PropertyChanged?.Invoke(
+                    this,
+                    new PropertyChangedEventArgs(nameof(CurrentTheme)));
+            }
+        }
+
+        public ThemeService()
+        {
+            _currentThemeDictionary =
+                Application.Current.Resources.MergedDictionaries
+                    .FirstOrDefault(d =>
+                        d.Source?.OriginalString.Contains("/Themes/") == true);
+        }
+
+        public void ApplyTheme(ThemeOptions themeName)
+        {
+            if (CurrentTheme == themeName)
+                return;
+
+            var newTheme = new ResourceDictionary
+            {
+                Source = new Uri(
+                    $"/AdminPanelUI;component/Resources/Themes/{themeName}.xaml",
+                    UriKind.Relative)
+            };
+
+            var dictionaries =
+                Application.Current.Resources.MergedDictionaries;
+
+            if (_currentThemeDictionary != null)
+            {
+                dictionaries.Remove(_currentThemeDictionary);
+            }
+
+            dictionaries.Add(newTheme);
+
+            _currentThemeDictionary = newTheme;
+
+            CurrentTheme = themeName;
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
     }
 }
