8cdd9edde2bb1d29284321535e542954697fd2e2
Amin Mirzaei <aminmirzaeidev@gmail.com>
2026-08-28 14:05:46 +0330

    Redesign ContactInfoRow with FontAwesome icons
    ContactInfoRow XAML now uses a Border and Grid layout with four columns for icon, title, value, and action icon, referencing FontAwesome.Sharp. Removed unused namespaces and empty Grid. Added Title, Value, Icon, and ActionIcon dependency properties in code-behind, with default icons set. Minor whitespace change in Main.xaml.

--- sources/AdminPanelUI/Controls/Composite/ContactInfoRow.xaml.cs
+++ sources/AdminPanelUI/Controls/Composite/ContactInfoRow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using FontAwesome.Sharp;
+using System;
 using System.Collections.Generic;
 using System.Text;
 using System.Windows;
@@ -22,5 +23,61 @@ namespace AdminPanelUI.Controls.Composite
         {
             InitializeComponent();
         }
+
+
+        public string Title
+        {
+            get => (string)GetValue(TitleProperty);
+            set => SetValue(TitleProperty, value);
+        }
+
+        public static readonly DependencyProperty TitleProperty =
+            DependencyProperty.Register(
+                nameof(Title),
+                typeof(string),
+                typeof(ContactInfoRow),
+                new PropertyMetadata(string.Empty));
+
+
+        public string Value
+        {
+            get => (string)GetValue(ValueProperty);
+            set => SetValue(ValueProperty, value);
+        }
+
+        public static readonly DependencyProperty ValueProperty =
+            DependencyProperty.Register(
+                nameof(Value),
+                typeof(string),
+                typeof(ContactInfoRow),
+                new PropertyMetadata(string.Empty));
+
+
+        public IconChar Icon
+        {
+            get => (IconChar)GetValue(IconProperty);
+            set => SetValue(IconProperty, value);
+        }
+
+        public static readonly DependencyProperty IconProperty =
+            DependencyProperty.Register(
+                nameof(Icon),
+                typeof(IconChar),
+                typeof(ContactInfoRow),
+                new PropertyMetadata(IconChar.InfoCircle));
+
+
+        public IconChar ActionIcon
+        {
+            get => (IconChar)GetValue(ActionIconProperty);
+            set => SetValue(ActionIconProperty, value);
+        }
+
+        public static readonly DependencyProperty ActionIconProperty =
+            DependencyProperty.Register(
+                nameof(ActionIcon),
+                typeof(IconChar),
+                typeof(ContactInfoRow),
+                new PropertyMetadata(IconChar.ExternalLinkAlt));
     }
 }
