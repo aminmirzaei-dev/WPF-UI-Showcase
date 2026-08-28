4356ced94a859bd4b730ace9c6b1452cf7318e96
Amin Mirzaei <aminmirzaeidev@gmail.com>
2026-08-28 13:58:18 +0330

    Refactor FeatureCard with styled layout and bindings
    Updated FeatureCard.xaml to use a styled Border and two-column Grid layout with FontAwesome icon and text blocks for Title and Description. Added dependency properties for Title, Description, and Icon (IconChar) in FeatureCard.xaml.cs to support data binding. Included necessary FontAwesome.Sharp references.

--- sources/AdminPanelUI/Controls/Composite/FeatureCard.xaml.cs
+++ sources/AdminPanelUI/Controls/Composite/FeatureCard.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using FontAwesome.Sharp;
+using System;
 using System.Collections.Generic;
 using System.Text;
 using System.Windows;
@@ -22,5 +23,46 @@ namespace AdminPanelUI.Controls.Composite
         {
             InitializeComponent();
         }
+
+        public string Title
+        {
+            get => (string)GetValue(TitleProperty);
+            set => SetValue(TitleProperty, value);
+        }
+
+        public static readonly DependencyProperty TitleProperty =
+            DependencyProperty.Register(
+                nameof(Title),
+                typeof(string),
+                typeof(FeatureCard),
+                new PropertyMetadata(string.Empty));
+
+
+        public string Description
+        {
+            get => (string)GetValue(DescriptionProperty);
+            set => SetValue(DescriptionProperty, value);
+        }
+
+        public static readonly DependencyProperty DescriptionProperty =
+            DependencyProperty.Register(
+                nameof(Description),
+                typeof(string),
+                typeof(FeatureCard),
+                new PropertyMetadata(string.Empty));
+
+
+        public IconChar Icon
+        {
+            get => (IconChar)GetValue(IconProperty);
+            set => SetValue(IconProperty, value);
+        }
+
+        public static readonly DependencyProperty IconProperty =
+            DependencyProperty.Register(
+                nameof(Icon),
+                typeof(IconChar),
+                typeof(FeatureCard),
+                new PropertyMetadata(IconChar.Star));
     }
 }
