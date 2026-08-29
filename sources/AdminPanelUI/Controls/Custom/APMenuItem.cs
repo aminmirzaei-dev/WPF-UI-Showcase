be984382f4789deb273d6085775de66c6266adcd
Amin Mirzaei <aminmirzaeidev@gmail.com>
2026-08-29 17:49:04 +0330

    Add menuButtonActive style and APMenuItem class
    Added menuButtonActive style to Buttons.xaml for Button controls with a LinearGradientBrush background. Introduced APMenuItem class in AdminPanelUI.Controls.Custom, inheriting from RadioButton without additional functionality.

---
+++ sources/AdminPanelUI/Controls/Custom/APMenuItem.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AdminPanelUI.Controls.Custom
+{
+    public class APMenuItem : System.Windows.Controls.RadioButton
+    {
+    }
+}
