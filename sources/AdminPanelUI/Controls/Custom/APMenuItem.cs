bf37ed61bf9cb5c2e79e23fc6922696c6131d50c
Amin Mirzaei <aminmirzaeidev@gmail.com>
2026-08-30 01:35:50 +0330

    Refactor menu buttons to use APMenuItem control
    Replaced custom-styled Button menu items with new APMenuItem control inheriting from RadioButton. Added Icon dependency property for direct FontAwesome binding. Updated Buttons.xaml with a style and ControlTemplate for APMenuItem, featuring animated states and integrated icon/text layout. Updated Workspace.xaml to use APMenuItem, removing redundant StackPanels/TextBlocks. Removed menuButtonText style from TextBlocks.xaml. Centralizes menu button logic, simplifies XAML, and improves visual feedback.

--- sources/AdminPanelUI/Controls/Custom/APMenuItem.cs
+++ sources/AdminPanelUI/Controls/Custom/APMenuItem.cs
@@ -1,10 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using FontAwesome.Sharp;
+using System.Windows;
 
 namespace AdminPanelUI.Controls
 {
     public class APMenuItem : System.Windows.Controls.RadioButton
     {
+        #region Icon
+
+        public static readonly DependencyProperty IconProperty =
+            DependencyProperty.Register(
+                nameof(Icon),
+                typeof(IconChar),
+                typeof(APMenuItem),
+                new FrameworkPropertyMetadata(IconChar.None));
+
+        public IconChar Icon
+        {
+            get => (IconChar)GetValue(IconProperty);
+            set => SetValue(IconProperty, value);
+        }
+
+        #endregion
+
+
     }
 }
