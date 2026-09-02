774c7f018d3e4a63dc57a288cf2b5354b17941a3
Amin Mirzaei <aminmirzaeidev@gmail.com>
2026-09-02 04:12:31 +0330

    Add APComboBox custom control and styles
    Created APComboBox.cs inheriting ComboBox with a custom default style key. Added ComboBoxs.xaml for APComboBox styles and templates. Registered ComboBoxs.xaml in App.xaml for global style access.

---
+++ sources/AdminPanelUI/Controls/Custom/APComboBox.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace AdminPanelUI.Controls
+{
+    public class APComboBox : ComboBox
+    {
+        static APComboBox()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(
+                typeof(APComboBox),
+                new FrameworkPropertyMetadata(typeof(APComboBox)));
+        }
+    }
+}
