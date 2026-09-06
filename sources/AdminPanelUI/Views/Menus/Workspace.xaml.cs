0378ca38cf95da6fab7b8955d9885bc00936f3ca
Amin Mirzaei <aminmirzaeidev@gmail.com>
2026-09-06 05:26:35 +0330

    Add enum-based navigation system to Workspace
    Implement page navigation in Workspace using AppPages enum and ContentPresenter. Add event handlers for menu items to switch pages. Enable INotifyPropertyChanged for CurrentPage to support data binding. Update Main.xaml to use dynamic ContentPresenter instead of static Dashboard page.

--- sources/AdminPanelUI/Views/Menus/Workspace.xaml.cs
+++ sources/AdminPanelUI/Views/Menus/Workspace.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using AdminPanelUI.Views.Dialogs;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Security.AccessControl;
 using System.Text;
 using System.Windows;
 using System.Windows.Controls;
@@ -14,17 +16,85 @@ using System.Windows.Shapes;
 
 namespace AdminPanelUI.Views.Menus
 {
+    public enum AppPages
+    {
+        Dashboard, Listing, Messages, Settings, About
+    }
+
+
     /// <summary>
     /// Interaction logic for Workspace.xaml
     /// </summary>
     public partial class Workspace : UserControl
     {
 
+        private Views.Pages.Dashboard dashboardPage = new Views.Pages.Dashboard();
+        private Views.Pages.Listing listingPage = new Views.Pages.Listing();
+        private Views.Pages.Messages messagesPage = new Views.Pages.Messages();
+        private Views.Pages.Settings settingsPage = new Views.Pages.Settings();
+        private Views.Pages.About aboutPage = new Views.Pages.About();
+
+        
+
+
         public Workspace()
         {
             InitializeComponent();
-            
 
+            this.CurrentPage = AppPages.Dashboard;
+
+
+        }
+
+        public ContentPresenter ContentPage { get; set; } = new ContentPresenter();
+
+        private AppPages _currentPage;
+
+        public AppPages CurrentPage
+        {
+            get => _currentPage;
+            set
+            {
+                if (_currentPage == value)
+                    return;
+
+                _currentPage = value;
+                OnPropertyChanged(nameof(CurrentPage));
+            }
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        private void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(
+                this,
+                new PropertyChangedEventArgs(propertyName));
+        }
+
+        public void ExecutePage(AppPages page)
+        {
+
+            this.CurrentPage = page;
+
+            switch (page)
+            {
+                case AppPages.Dashboard:
+                    this.ContentPage.Content = dashboardPage;
+                    break;
+                case AppPages.Listing:
+                    this.ContentPage.Content = listingPage;
+                    break;
+                case AppPages.Messages:
+                    this.ContentPage.Content = messagesPage;
+                    break;
+                case AppPages.Settings:
+                    this.ContentPage.Content = settingsPage;
+                    break;
+                case AppPages.About:
+                    this.ContentPage.Content = aboutPage;
+                    break;
+            }
         }
 
 
@@ -34,6 +104,26 @@ namespace AdminPanelUI.Views.Menus
             exitDialog.Owner = Window.GetWindow(this);
 
             exitDialog.ShowDialog();
+        }
+
+        private void DashboardMenuItem_Checked(object sender, RoutedEventArgs e)
+        {
+            this.ExecutePage(AppPages.Dashboard);
+        }
+
+        private void ListingMenuItem_Checked(object sender, RoutedEventArgs e)
+        {
+            this.ExecutePage(AppPages.Listing);
+        }
+
+        private void MessagesMenuItem_Checked(object sender, RoutedEventArgs e)
+        {
+            this.ExecutePage(AppPages.Messages);
+        }
+
+        private void AboutUsMenuItem_Checked(object sender, RoutedEventArgs e)
+        {
+            this.ExecutePage(AppPages.About);
         }
     }
 }
