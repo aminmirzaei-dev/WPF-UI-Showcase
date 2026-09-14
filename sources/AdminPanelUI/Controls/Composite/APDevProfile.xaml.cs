8eaabaff1fd7c0b61668e9a54def41a41cf30cfc
Amin Mirzaei <aminmirzaeidev@gmail.com>
2026-09-15 02:03:22 +0330

    Remove theme and localization; unify dev profile image
    Removed all theme and localization support, including ThemeService, LocalizationService, resource dictionaries, and resource files. Deleted fantasy developer images and references. Replaced "Amin.jpg" with "AminDevProfile.jpg" throughout. Updated APDevProfile control and pages to use the static image without theming or localization. Cleaned up App.xaml resource merges.

--- sources/AdminPanelUI/Controls/Composite/APDevProfile.xaml.cs
+++ sources/AdminPanelUI/Controls/Composite/APDevProfile.xaml.cs
@@ -1,16 +1,5 @@
-﻿using AdminPanelUI.Services;
-using System;
-using System.Collections.Generic;
-using System.Text;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace AdminPanelUI.Controls
 {
@@ -19,14 +8,6 @@ namespace AdminPanelUI.Controls
     /// </summary>
     public partial class APDevProfile : UserControl
     {
-
-        public static readonly DependencyProperty ThemeProperty = DependencyProperty.Register("Theme", typeof(AdminPanelUI.Services.ThemeOptions), typeof(APDevProfile));
-        public ThemeOptions Theme
-        {
-            get => (ThemeOptions)GetValue(ThemeProperty);
-            set => SetValue(ThemeProperty, value);
-        }
-
 
         public APDevProfile()
         {
