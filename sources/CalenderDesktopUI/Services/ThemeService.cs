b1f8fe2f6e1539f521254ece0b8d3d68d3a4ed16
Amin Mirzaei <aminmirzaeidev@gmail.com>
2026-09-22 02:41:01 +0330

    Refactor ThemeService for public use and theme switching
    Refactored ThemeService to a public class implementing INotifyPropertyChanged. Introduced ThemeOptions enum for Light and Dark themes. Added CurrentTheme property with change notification and ApplyTheme method to update WPF ResourceDictionaries. Enabled data binding support via PropertyChanged event.

--- sources/CalenderDesktopUI/Services/ThemeService.cs
+++ sources/CalenderDesktopUI/Services/ThemeService.cs
@@ -1,10 +1,74 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Text;
+using System.Windows;
 
 namespace CalenderDesktopUI.Services
 {
-    internal class ThemeService
+    public enum ThemeOptions
     {
+        Light, Dark
+    }
+
+    public class ThemeService : INotifyPropertyChanged
+    {
+        private ThemeOptions _currentTheme = ThemeOptions.Light;
+
+        private ResourceDictionary? _currentThemeDictionary;
+
+        public ThemeOptions CurrentTheme
+        {
+            get => _currentTheme;
+
+            private set
+            {
+                if (_currentTheme == value)
+                    return;
+
+                _currentTheme = value;
+
+                PropertyChanged?.Invoke(
+                    this,
+                    new PropertyChangedEventArgs(nameof(CurrentTheme)));
+            }
+        }
+
+        public ThemeService()
+        {
+            _currentThemeDictionary =
+                Application.Current.Resources.MergedDictionaries
+                    .FirstOrDefault(d =>
+                        d.Source?.OriginalString.Contains("/Themes/") == true);
+        }
+
+        public void ApplyTheme(ThemeOptions themeName)
+        {
+            if (CurrentTheme == themeName)
+                return;
+
+            var newTheme = new ResourceDictionary
+            {
+                Source = new Uri(
+                    $"/CalendarDesktopUI;component/Resources/Themes/{themeName}.xaml",
+                    UriKind.Relative)
+            };
+
+            var dictionaries =
+                Application.Current.Resources.MergedDictionaries;
+
+            if (_currentThemeDictionary != null)
+            {
+                dictionaries.Remove(_currentThemeDictionary);
+            }
+
+            dictionaries.Add(newTheme);
+
+            _currentThemeDictionary = newTheme;
+
+            CurrentTheme = themeName;
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
     }
 }
