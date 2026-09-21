87517d46b8d6d0a33c039586d1d23bddb522bb87
Amin Mirzaei <aminmirzaeidev@gmail.com>
2026-09-22 02:44:22 +0330

    Add PaletteService with palette switching support
    Introduced PaletteOptions enum for color selection. Made PaletteService public and implement INotifyPropertyChanged to manage palette changes. Added ApplyPalette method to update application resources via ResourceDictionary. Raised PropertyChanged event on palette change. Utilized System.ComponentModel and System.Windows namespaces.

--- sources/CalenderDesktopUI/Services/PaletteService.cs
+++ sources/CalenderDesktopUI/Services/PaletteService.cs
@@ -1,10 +1,75 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Text;
+using System.Windows;
 
 namespace CalenderDesktopUI.Services
 {
-    internal class PaletteService
+    public enum PaletteOptions
     {
+        Red, Blue, Green, Yellow, Purple, Orange, Pink
+    }
+
+
+    public class PaletteService : INotifyPropertyChanged
+    {
+        private PaletteOptions _currentPalette = PaletteOptions.Red;
+
+        private ResourceDictionary? _currentPaletteDictionary;
+
+        public PaletteOptions CurrentPalette
+        {
+            get => _currentPalette;
+
+            private set
+            {
+                if (_currentPalette == value)
+                    return;
+
+                _currentPalette = value;
+
+                PropertyChanged?.Invoke(
+                    this,
+                    new PropertyChangedEventArgs(nameof(CurrentPalette)));
+            }
+        }
+
+        public PaletteService()
+        {
+            _currentPaletteDictionary =
+                Application.Current.Resources.MergedDictionaries
+                    .FirstOrDefault(d =>
+                        d.Source?.OriginalString.Contains("/Palettes/") == true);
+        }
+
+        public void ApplyPalette(PaletteOptions paletteName)
+        {
+            if (CurrentPalette == paletteName)
+                return;
+
+            var newPalette = new ResourceDictionary
+            {
+                Source = new Uri(
+                    $"/CalendarDesktopUI;component/Resources/Palettes/{paletteName}.xaml",
+                    UriKind.Relative)
+            };
+
+            var dictionaries =
+                Application.Current.Resources.MergedDictionaries;
+
+            if (_currentPaletteDictionary != null)
+            {
+                dictionaries.Remove(_currentPaletteDictionary);
+            }
+
+            dictionaries.Add(newPalette);
+
+            _currentPaletteDictionary = newPalette;
+
+            CurrentPalette = paletteName;
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
     }
 }
