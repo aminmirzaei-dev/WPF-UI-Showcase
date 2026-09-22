03dc51f5bdadf041c304b7c8cd0d5ca18f6af75e
Amin Mirzaei <aminmirzaeidev@gmail.com>
2026-09-23 02:16:21 +0330

    Add CDCalendar class and update project file
    Added CDCalendar class inheriting from Calendar in Controls.Custom. Updated using directives. Removed "Controls\Custom\" folder from CalendarDesktopUI.csproj.

---
+++ sources/CalenderDesktopUI/Controls/Custom/CDCalendar.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CalendarDesktopUI.Controls.Custom
+{
+    internal class CDCalendar : System.Windows.Controls.Calendar
+    {
+    }
+}
