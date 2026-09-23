46cdd416d85d83ce1177df7d055e75d48a4fa1df
Amin Mirzaei <aminmirzaeidev@gmail.com>
2026-09-23 12:44:41 +0330

    Replace MenuButton with MenuButtonItem in TaskItem
    Replaced controls:MenuButton with local:MenuButtonItem in TaskItem.xaml. Updated MenuButtonItem.cs to inherit from Button and added Caption and Icon dependency properties. Added a new style for MenuButtonItem in Buttons.xaml with FontAwesome icon support and improved layout and mouse-over behavior.

--- sources/CalenderDesktopUI/Controls/Custom/MenuButtonItem.cs
+++ sources/CalenderDesktopUI/Controls/Custom/MenuButtonItem.cs
@@ -1,10 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Windows;
 
 namespace CalendarDesktopUI.Controls
 {
-    internal class MenuButtonItem
+    internal class MenuButtonItem : System.Windows.Controls.Button
     {
+
+        public static readonly DependencyProperty CaptionProperty = DependencyProperty.Register("Caption", typeof(string), typeof(MenuButtonItem));
+        public static readonly DependencyProperty IconProperty = DependencyProperty.Register("Icon", typeof(FontAwesome.WPF.FontAwesomeIcon), typeof(MenuButtonItem));
+
+        public string Caption
+        {
+            get { return (string)GetValue(CaptionProperty); }
+            set { SetValue(CaptionProperty, value); }
+        }
+
+        public FontAwesome.WPF.FontAwesomeIcon Icon
+        {
+            get { return (FontAwesome.WPF.FontAwesomeIcon)GetValue(IconProperty); }
+            set { SetValue(IconProperty, value); }
+        }
     }
 }
